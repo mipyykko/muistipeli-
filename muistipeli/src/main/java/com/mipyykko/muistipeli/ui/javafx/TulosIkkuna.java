d1d59ab42ba9b752d0235e521a0c034fbf45c395
Mikko Pyykkö <mipyykko@gmail.com>
2017-02-24 13:50:54 +0200

    tulosikkuna

--- muistipeli/src/main/java/com/mipyykko/muistipeli/ui/javafx/TulosIkkuna.java
+++ muistipeli/src/main/java/com/mipyykko/muistipeli/ui/javafx/TulosIkkuna.java
@@ -5,10 +5,30 @@
  */
 package com.mipyykko.muistipeli.ui.javafx;
 
+import javafx.geometry.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
 /**
  *
  * @author pyykkomi
  */
-public class TulosIkkuna {
+public class TulosIkkuna extends GridPane {
+
+    private Stage primaryStage;
+    private GridPane sisalto;
     
+    public TulosIkkuna(Stage primaryStage) {
+        super();
+        this.primaryStage = primaryStage;
+
+        sisalto = new GridPane();
+        setAlignment(Pos.CENTER);
+        setPadding(new Insets(15));
+        setHgap(16);
+        setVgap(8);
+        setBackground(new Background(new BackgroundFill(Color.YELLOW, null, null)));
+
+    }
 }
